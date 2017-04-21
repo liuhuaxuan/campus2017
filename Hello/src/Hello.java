ea9cd15532f9524e73f590ca03da205fce85aed8
liuhuaxuan <1129655300@qq.com>
2017-04-21 19:54:26 +0800

    hbx

---
+++ Hello/src/Hello.java
@@ -0,0 +1,14 @@
+
+
+/**
+ * Created by lhx on 17-4-21.
+ */
+public class Hello {
+
+
+    public static void main(String[] args) {
+
+                System.out.println("该java文件有效代码行数:" );
+
+    }
+}
